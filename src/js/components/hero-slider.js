a9f1438c81ff35ccc21eb09abe6ed8df7abaa70c
BAO <andriy.bezkorovajnyj@gmail.com>
2021-11-07 18:48:31 +0200

    done

---
+++ src/js/components/hero-slider.js
@@ -0,0 +1,13 @@
+
+import Swiper from '../vendor/swiper-bundle.js';
+
+  let swiper = new Swiper(".swiper__hero", {
+   
+  scrollbar: {
+  el: ".swiper-scrollbar",
+   hide: true,
+ }, 
+});
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,4 @@
 import './vendor/focus-visible.min.js';
 import './components/burger';
+import './components/hero-slider';
+
